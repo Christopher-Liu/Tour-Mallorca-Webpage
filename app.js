d7b7ac955d96128fb14e817822d553b293cc5e00
Christopher-Liu <christopherbrianliu@gmail.com>
2017-07-26 17:18:57 -0700

    Main page (index.ejs) adjustments
    - Put all content into the main page, following the site's redesign
    - Changed up some of the CSS to fit the site's new design as well
    - Jumbotron has been made smaller
    - Jumbotron text has been repositioned and has a transparent background
    - Refactored some of the CSS to rely less on ID's and slower selectors,
    preferring simpler selectors and more use of classes

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var Comment    = require('./models/comments.js');
 
 var app = express();
 
-
+/*
 // Have to set the username and password before connection to the server works
 var username = '';
 var password = '';
 // Connecting to the database servers on MongoDB Atlas
 mongoose.connect('mongodb://' + username + ':' + password + '@cluster0-shard-00-00-cmlex.mongodb.net:27017,cluster0-shard-00-01-cmlex.mongodb.net:27017,cluster0-shard-00-02-cmlex.mongodb.net:27017/mallorca?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+*/
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
