dba9f2fec34289e45c3c2a54fdd8b3a6e018325d
Christopher-Liu <christopherbrianliu@gmail.com>
2017-07-26 18:50:49 -0700

    Added in a thumbnail section to the index.ejs page
    - Added in three thumbnails that will appear in the frontpage
    - The three thumbnails will pull from the vacations database
    - It will only pull the first 3 results from the vacations database, as
    only 3 thumbnails will appear on the front page
    - The database will hold all of the "vacations packages", more than the
    current 3, which will be used to populate the second page (which will be
    a list of all the vacations)
    
    - Since this is no longer the "real" index, considering renaming the
    file to home.ejs
    - Also, code to seed the database is left in the app.js file as this is
    still in development. This will be removed later on
    - All of the thumbnails currently use an intermediary placeholder.png
    image- each vacation document in the database will have their own image
    served

--- app.js
+++ app.js
@@ -6,13 +6,54 @@ var Comment    = require('./models/comments.js');
 
 var app = express();
 
-/*
+
 // Have to set the username and password before connection to the server works
 var username = '';
 var password = '';
 // Connecting to the database servers on MongoDB Atlas
 mongoose.connect('mongodb://' + username + ':' + password + '@cluster0-shard-00-00-cmlex.mongodb.net:27017,cluster0-shard-00-01-cmlex.mongodb.net:27017,cluster0-shard-00-02-cmlex.mongodb.net:27017/mallorca?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
-*/
+
+
+// Code for seeding the database each time we start the server (will only be here during development)
+var data = [
+    {
+        name: 'Cloud Rest',
+        image: '/img/placeholder.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod dolor quis metus mollis ultricies. Pellentesque porta, mauris sit amet ullamcorper tincidunt, lorem diam elementum libero, tempus rhoncus orci libero in ex. Etiam luctus ultrices neque eu consequat. Nullam condimentum vehicula sodales. Integer ullamcorper neque sed turpis mattis, eu porta lorem maximus. Vestibulum augue mauris, vehicula nec nisi nec, porta bibendum nibh.'
+    },
+    {
+        name: 'Dope Rest',
+        image: '/img/placeholder.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod dolor quis metus mollis ultricies. Pellentesque porta, mauris sit amet ullamcorper tincidunt, lorem diam elementum libero, tempus rhoncus orci libero in ex. Etiam luctus ultrices neque eu consequat. Nullam condimentum vehicula sodales. Integer ullamcorper neque sed turpis mattis, eu porta lorem maximus. Vestibulum augue mauris, vehicula nec nisi nec, porta bibendum nibh.'
+    },
+    {
+        name: 'Sick Rest',
+        image: '/img/placeholder.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod dolor quis metus mollis ultricies. Pellentesque porta, mauris sit amet ullamcorper tincidunt, lorem diam elementum libero, tempus rhoncus orci libero in ex. Etiam luctus ultrices neque eu consequat. Nullam condimentum vehicula sodales. Integer ullamcorper neque sed turpis mattis, eu porta lorem maximus. Vestibulum augue mauris, vehicula nec nisi nec, porta bibendum nibh.'
+    }
+];
+
+
+Vacation.remove({}, function(err) {
+  if (err) {
+    console.log('deleted all vacations');
+  } else {
+    data.forEach(function(seed){
+      Vacation.create(seed, function(err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          result.save();
+          console.log('added in vacation')
+        }
+      });
+    });
+  }
+});
+
+
+
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,14 +61,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
-
-
 // =========
 // Routes
 // =========
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  // We limit the query to 3 results since the first page displays only 3 results
+  Vacation.find({}).limit(3).exec(function(err, allVacations) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index.ejs', {vacations: allVacations});
+    }
+  });
 });
+
 
 app.get('/about', function(req, res){
   res.render('about.ejs');
